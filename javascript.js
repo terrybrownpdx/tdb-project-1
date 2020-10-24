5db6e19e819ff27ffeb05a99b6b9c58621198b54
tdb <tbrow69@msn.com>
2020-10-24 09:15:11 -0700

    updated

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	var clientID = "MjEzNjE5NjJ8MTYwMzMzNDkwMi4zNTQ2ODkx";
 	var queryURL = "https://api.seatgeek.com/2/events?client_id=" + clientID;
 
+	//function searchSeatGeeks(event){
 	$.ajax({
 		url: queryURL,
 		method: "GET",
@@ -9,41 +10,75 @@ $(document).ready(function () {
 		console.log(response);
 		console.log(queryURL);
 
+		var city = $(".city").text("City: " + response.events);
+		var event = $("event").text("Event " + response.events);
+		var date = $("date").text("Date " + response.events);
 
-	//$(".city").text("City: " + response.events.array.title);
-	$("event").text("Event " + response.events.title);
-	//$("date").text("Date " + response.events.time_tbd );
+		console.log("City: " + JSON.stringify(response.events[0].venue.city));
+		console.log("Event " + JSON.stringify(response.events[0].title));
+		console.log("Date " + JSON.stringify(response.events[0].announce_date));
 
-	//console.log("City: " + response.events);
-	console.log("event " + response.events.title);
-	//console.log("Date " + response.events.time_tbd );
+		for (var i = 0; i < events.venue.city.length ; i ++){
+			console.log(events[i])
+		}
+
+		$("#event-info").empty();
+		$("#event-info").append(city, event, date);
+	});
+	
+		// Event handler for user clicking the select-artist button
+		$("#select-event").on("click", function (event) {
+			// Preventing the button from trying to submit the form
+			event.preventDefault();
+			// Storing the artist name
+			var cityInput = $("#city-input").val().trim();
+			var eventInput = $("#event-input").val().trim();
+			var dateInput = $("#date-input").val().trim();
+
+		// Running the searchBandsInTown function(passing in the artist as an argument)
+		//searchSeatGeeks(cityInput, eventInput, dateInput);
+
+
+		})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// var apiKey = "5c2c54600emsh45fe6fce3e37e47p19f3d9jsn393344917d9e";
+		// var queryURL = "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm";
+		//  $.ajax ({
+		//  	url: queryURL,
+		//  	method: "GET",
+		//  }).then(function(response){
+		// 	console.log(response);
+		//  	console.log(queryURL);
+		//  })
+
+		// const settings = {
+		// 	"async": true,
+		// 	"crossDomain": true,
+		// 	"url": "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm",
+		// 	"method": "GET",
+		// 	"headers": {
+		// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+		// 		"x-rapidapi-key": "5c2c54600emsh45fe6fce3e37e47p19f3d9jsn393344917d9e"
+		// 	}
+		// };
+
+		// $.ajax(settings).done(function (response) {
+		// 	console.log(response);
+		// });
+
+
+
 	})
-
-	// var apiKey = "5c2c54600emsh45fe6fce3e37e47p19f3d9jsn393344917d9e";
-	// var queryURL = "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm";
-	//  $.ajax ({
-	//  	url: queryURL,
-	//  	method: "GET",
-	//  }).then(function(response){
-	// 	console.log(response);
-	//  	console.log(queryURL);
-	//  })
-
-	// const settings = {
-	// 	"async": true,
-	// 	"crossDomain": true,
-	// 	"url": "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm",
-	// 	"method": "GET",
-	// 	"headers": {
-	// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-	// 		"x-rapidapi-key": "5c2c54600emsh45fe6fce3e37e47p19f3d9jsn393344917d9e"
-	// 	}
-	// };
-
-	// $.ajax(settings).done(function (response) {
-	// 	console.log(response);
-	// });
-
-
-
-})
